5577356947053d24fc9c856de4b024ed53edb418
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-10-21 22:58:20 +0200

    Signed Messages

--- src/frontend/workspaces/NetworkWorkspace/NetworkWorkspace.js
+++ src/frontend/workspaces/NetworkWorkspace/NetworkWorkspace.js
@@ -1,5 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ethers } from 'ethers'
 
-const NetworkWorkspace = () => <h1>Network Workspace</h1>
+import './NetworkWorkspace.sass'
+
+const NetworkWorkspace = () => {
+  const [ signature, setSignature ] = useState('')
+
+  const signMessage = async e => {
+    e.preventDefault()
+    await window.ethereum.request({ method: 'eth_requestAccounts' })
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    setSignature(await signer.signMessage(e.target.message.value))
+  }
+
+  return (
+    <div id='NetworkWorkspace'>
+      <h1>Network Workspace</h1>
+
+      <form onSubmit={signMessage}>
+        <input id='message'></input>
+        <button type='submit'>Sign Message</button>
+      </form>
+
+      <p>{signature}</p>
+    </div>
+  )
+}
 
 export default NetworkWorkspace
